52ec1074bcb04ee7747aef498e9e2c13c38c1a18
mitermitek <188499468+mitermitek@users.noreply.github.com>
2025-12-02 20:23:42 +0100

    refactor: remove UserController and related request classes

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,9 @@
 
 use App\Http\Controllers\Token\CreateTokenController;
 use App\Http\Controllers\Token\RevokeCurrentTokenController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('tokens')->group(function () {
     Route::post('', CreateTokenController::class);
     Route::delete('', RevokeCurrentTokenController::class)->middleware('auth:sanctum');
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('users')->group(function () {
-        Route::get('/', [UserController::class, 'index']);
-        Route::post('/', [UserController::class, 'store']);
-        Route::get('/{id}', [UserController::class, 'show']);
-        Route::put('/{id}', [UserController::class, 'update']);
-        Route::delete('/{id}', [UserController::class, 'destroy']);
-    });
 });
